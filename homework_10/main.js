7a60713289f636b106694d0e4be0e64286cdd584
Dmytro Malashchuk <dmytro.malashchuk@nure.ua>
2020-12-20 15:48:59 +0200

    commit num 300

--- homework_10/main.js
+++ homework_10/main.js
@@ -1,25 +1,10 @@
-const noteDo = new Audio('sounds/note_do.mp3');
-const noteRe = new Audio('sounds/note_re.mp3');
-const noteMi = new Audio('sounds/note_mi.mp3');
-const noteFa = new Audio('sounds/note_fa.mp3');
-const noteSol = new Audio('sounds/note_sol.mp3');
-const noteLja = new Audio('sounds/note_lja.mp3');
-const noteSi = new Audio('sounds/note_si.mp3');
-document.addEventListener('keydown', function(e){
-    switch(e.code){
-        case 'Digit1': 
-    noteDo.currentTime = 0; noteDo.play(); break;
-        case 'Digit2': 
-    noteRe.currentTime = 0;noteRe.play(); break;
-        case 'Digit3': 
-    noteMi.currentTime = 0;noteMi.play(); break;
-        case 'Digit4': 
-    noteFa.currentTime = 0;noteFa.play(); break;
-        case 'Digit5': 
-    noteSol.currentTime = 0;noteSol.play(); break;
-        case 'Digit6': 
-    noteLja.currentTime = 0;noteLja.play(); break;
-        case 'Digit7': 
-    noteSi.currentTime = 0;noteSi.play(); break;
-    }
+function playSound(e){
+    const code = e.keyCode ? e.keyCode: this.getAttribute("data-key");
+    const activeAudio = document.querySelector('audio[data-key="' + code + '"]');
+    activeAudio.currentTime = 0;
+    activeAudio.play();
+}
+document.addEventListener('keydown', playSound);
+document.querySelectorAll(".sounds").forEach(function (sound) {
+    sound.addEventListener('click', playSound);
 });
