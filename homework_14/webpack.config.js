a551b343ceed2aa0b920a7350f2ccc3c97c42825
Дмитрий Малащук <dmytro.malashchuk@nure.ua>
2021-01-08 05:12:29 +0200

    commit num 506

--- homework_14/webpack.config.js
+++ homework_14/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     entry: "./src/index.js",
     output:{
@@ -10,6 +11,10 @@ module.exports = {
             test: /\.js$/,
             exclude: /node_modules/,
             loader: "babel-loader"
+        },{
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
         }]
-    }
+    }, 
+    plugins: [new HtmlWebpackPlugin()]
 }
